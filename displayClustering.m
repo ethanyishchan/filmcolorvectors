0ff54cf0797ce1bbee7e175b2dfa82ee281a467f
John Lee <johnwenhaolee@gmail.com>
2015-11-07 18:57:40 -0800

    Cleaned up display cluster

--- displayClustering.m
+++ displayClustering.m
@@ -1,29 +1,18 @@
-numCat1 = sum((idx == 1));
-numCat2 = sum((idx == 2));
-
-currentCounter = 1;
-for i = 1:numCat1
-    while(idx(currentCounter) ~= 1)
+directoryName = '../archive/VectorImage/';
+for j = 1:k
+    figure;
+    currentCounter = 1;
+    numCat = sum((idx == j));
+    spDim = ceil(sqrt(numCat));
+    for i = 1:numCat
+        while(idx(currentCounter) ~= j)
+            currentCounter = currentCounter + 1;
+        end
+        imgName = (strcat(directoryName, cellstr(movieNames(currentCounter)),'.png'));
+        img = imread(imgName{1});
+        subplot(spDim, spDim, i);
+        imshow(img);
+        title(movieNames(currentCounter));
         currentCounter = currentCounter + 1;
     end
-    imgName = (strcat(cellstr(movieNames(currentCounter)),'.png'));
-    img = imread(imgName{1});
-    subplot(3, 4, i);
-    imshow(img);
-    title(movieNames(currentCounter));
-    currentCounter = currentCounter + 1;
-end
-
-figure;
-currentCounter = 1;
-for i = 1:numCat2
-    while(idx(currentCounter) ~= 2)
-        currentCounter = currentCounter + 1;
-    end
-    imgName = (strcat(cellstr(movieNames(currentCounter)),'.png'));
-    img = imread(imgName{1});
-    subplot(3, 4, i);
-    imshow(img);
-    title(movieNames(currentCounter));
-    currentCounter = currentCounter + 1;
 end
